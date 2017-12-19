3a8f05db17e53b7de208d1c71779e74cadad8caa
sol315 <lisong1013@gmail.com>
2017-12-18 22:44:02 -0500

    added server part

--- server/clear_data.sql
+++ server/clear_data.sql
@@ -1,6 +1,2 @@
-delete from features;
-delete from pictures;
-delete from cookies;
-ALTER TABLE features AUTO_INCREMENT = 1;
-ALTER TABLE pictures AUTO_INCREMENT = 1;
-ALTER TABLE cookies AUTO_INCREMENT = 1;
+delete from maintable;
+ALTER TABLE maintable AUTO_INCREMENT = 1;
