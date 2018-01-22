1f82207d95ddde953b01a5a87d92d21847beb452
Shujiang Wu <shujiangwu@dyn147077.cc.lehigh.edu>
2018-01-22 00:12:25 -0500

    只测试红色

--- client/fingerprint/texture/shader.fs.glsl
+++ client/fingerprint/texture/shader.fs.glsl
@@ -60,7 +60,8 @@ void main()
           color6 = floor((floor(weight1) * color5 + floor(weight2) * color4) / 1000.0 * 255.0 + 0.1) / 255.0;
           //gl_FragColor = vec4( w(x0) * w(y2) / 16384.0 - 0.5 , w(x2) * w(y3) / 16384.0 - 0.5, 0.0 , 1.0);
           
-          gl_FragColor = vec4( w( w (   w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) ) ) /  w ( w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) ) ) * 255.0) / 255.0 , w( w (   w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) ) + 31345.0 ) / 49152.0 * 255.0) / 255.0    , 0.0 , 1.0);
+          gl_FragColor = vec4( w( w (   w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) ) ) /  w ( w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) )  - 100.0 ) * 255.0) / 255.0 ,
+           0.0    , 0.0 , 1.0);
 
           //gl_FragColor = texture2D(sampler, vec2 (tx, ty));
           //gl_FragColor = texture2D(sampler, vec2 (wei_1 * text_point[i][0] + wei_2 * text_point[i+1][0] + wei_3 * text_point[i+2][0], wei_1 * text_point[i][1] + wei_2 * text_point[i+1][1] + wei_3 * text_point[i+2][1]));
