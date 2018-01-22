86eccac0973b2703fb54ea1d403d00b9a523d5fb
Shujiang Wu <shujiangwu@dyn147077.cc.lehigh.edu>
2018-01-22 11:55:23 -0500

    add flag

--- client/fingerprint/texture/shader.fs.glsl
+++ client/fingerprint/texture/shader.fs.glsl
@@ -9,13 +9,12 @@ uniform sampler2D sampler;
 
 void main()
 {
-  float x0, y0, x1, y1, z1, x2, y2, z2, x3,  y3, z3, z , tx, ty, tx0, ty0, tx1, ty1,tx2, ty2,tx3, ty3, j, weight1, weight2, bcs1, bcs2, bcs3, cs1, cs2, cs3;
+  float x0, y0, x1, y1, z1, x2, y2, z2, x3,  y3, z3, z , tx, ty, tx0, ty0, tx1, ty1,tx2, ty2,tx3, ty3, weight1, weight2, bcs1, bcs2, bcs3, cs1, cs2, cs3;
+  int flag;
   vec4 color0, color1, color2, color3, color4, color5, color6;
   x0 = gl_FragCoord.x * 1.0; y0 = gl_FragCoord.y * 1.0; z = -2.0;
-  j = 0.1;
   gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
   for (int i = 0 ; i < 510; i += 3){
-      j += 3.0;
       x1 = tri_point[i][0];   y1 = tri_point[i][1];   z1 = tri_point[i][2];
       x2 = tri_point[i+1][0]; y2 = tri_point[i+1][1]; z2 = tri_point[i+1][2];
       x3 = tri_point[i+2][0]; y3 = tri_point[i+2][1]; z3 = tri_point[i+2][2];
@@ -59,12 +58,22 @@ void main()
           weight2 = 1000.0 - round ((tx * 255.0 - floor(tx * 255.0))* 1000.0);
           color6 = floor((floor(weight1) * color5 + floor(weight2) * color4) / 1000.0 * 255.0 + 0.1) / 255.0;
           //gl_FragColor = vec4( w(x0) * w(y2) / 16384.0 - 0.5 , w(x2) * w(y3) / 16384.0 - 0.5, 0.0 , 1.0);
-          bcs1 =  w (   w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) ) );
+
+          bcs1 =  w (   w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) ) ); 
           cs1 = w (   w (w(x0) * w(y2)) + w (w(x2) * w(y3)) + w (w(x3) * w(y2)) - w (   w (w(x3) * w(y2)) + w (w(x2) * w(y0)) + w (w(x0) * w(y3)) )  );
+          flag = -1;
+          for (int j = 0; j < 1001; j++){
+            if ((flag == -1) && ( w(cs1 * float(j)) - w(bcs1 * 1000.0) > -0.1)){
+              flag = j;
+            }
+          }
+         
 
-          gl_FragColor = vec4( w( bcs1 / 16384.0 * 255.0) / 255.0 , w( cs1 / 16384.0 * 255.0) / 255.0    , 0.0 , 1.0);
+          
 
-          //gl_FragColor = vec4( w( w (bcs1  * 255.0) / cs1) / 255.0 , 0.0    , 0.0 , 1.0);
+          //gl_FragColor = vec4( w( bcs1 / 16384.0 * 255.0) / 255.0 , w( cs1 / 16384.0 * 255.0) / 255.0    , 0.0 , 1.0);
+
+          gl_FragColor = vec4( w(  w(float(flag)) / 100.0    * 255.0 ) / 255.0 , 0.0    , 0.0 , 1.0);
 
           //gl_FragColor = texture2D(sampler, vec2 (tx, ty));
           //gl_FragColor = texture2D(sampler, vec2 (wei_1 * text_point[i][0] + wei_2 * text_point[i+1][0] + wei_3 * text_point[i+2][0], wei_1 * text_point[i][1] + wei_2 * text_point[i+1][1] + wei_3 * text_point[i+2][1]));
