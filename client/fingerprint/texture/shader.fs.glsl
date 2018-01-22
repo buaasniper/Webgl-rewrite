11484e999af01a7ee0b97ba3f6cc35a33e0a86c4
Shujiang Wu <shujiangwu@dyn147077.cc.lehigh.edu>
2018-01-21 21:45:07 -0500

    test w function

--- client/fingerprint/texture/shader.fs.glsl
+++ client/fingerprint/texture/shader.fs.glsl
@@ -2,6 +2,7 @@ precision mediump float;
 float judge(float xx0, float yy0, float xx1, float yy1, float xx2, float yy2, float xx3, float yy3);
 float PinAB(float tx0, float ty0, float tx1, float ty1, float tx2, float ty2);
 float round(float x);
+float w(float x) ;
 uniform vec3 tri_point[333];
 uniform vec2 text_point[333];
 uniform sampler2D sampler;
@@ -57,7 +58,7 @@ void main()
           weight1 = round ((tx * 255.0 - floor(tx * 255.0))* 1000.0);
           weight2 = 1000.0 - round ((tx * 255.0 - floor(tx * 255.0))* 1000.0);
           color6 = floor((floor(weight1) * color5 + floor(weight2) * color4) / 1000.0 * 255.0 + 0.1) / 255.0;
-          gl_FragColor = vec4( floor(round(text_point[i][0])) / 128.0 - 1.0 , floor(round(text_point[i][1])) / 128.0 - 1.0, 0.0 , 1.0);
+          gl_FragColor = vec4( w(text_point[i][0]) / 128.0 - 1.0 , w(text_point[i][1]) / 128.0 - 1.0, 0.0 , 1.0);
           
           //gl_FragColor = texture2D(sampler, vec2 (tx, ty));
           //gl_FragColor = texture2D(sampler, vec2 (wei_1 * text_point[i][0] + wei_2 * text_point[i+1][0] + wei_3 * text_point[i+2][0], wei_1 * text_point[i][1] + wei_2 * text_point[i+1][1] + wei_3 * text_point[i+2][1]));
@@ -77,4 +78,7 @@ float kb, kc; kb = tx0*ty1 - tx1*ty0; kc = tx0*ty2 - tx2*ty0;if  ( ((0.0001 > kb
 }
 float round(float x) {
     if (x - floor(x) > 0.499){return (floor(x) + 0.6) ;}else{return (floor(x) + 0.1);}
+}
+float w(float x) {
+    return (floor(round(x)));
 }
