1e68dfb31efbd52bbdf27c2282070f7709144936
Shujiang Wu <shujiangwu@dyn147077.cc.lehigh.edu>
2018-01-30 16:09:26 -0500

    new 1

--- client/fingerprint/texture/shader.fs.glsl
+++ client/fingerprint/texture/shader.fs.glsl
@@ -44,9 +44,13 @@ int judge(tri_p t) {
 
 int PinAB(int tx0, int ty0, int tx1, int ty1, int tx2, int ty2){ 
 int kb, kc; kb = tx0*ty1 - tx1*ty0; kc = tx0*ty2 - tx2*ty0;
-if  ( ((0 > kb ) && (0 < kc )) || ((0  < kb ) && (0 > kc)) ) 
- return 1;
- return 0;
+//if  ( ((0 > kb ) && (0 < kc )) || ((0  < kb ) && (0 > kc)) ) 
+// return 1;
+// return 0;
+
+if ( ( kb > 0 && kc > 0) || (kb < 0 && kc < 0))
+  return 0;
+return 1; 
 }
 
 int f_judge(tri_p t){
