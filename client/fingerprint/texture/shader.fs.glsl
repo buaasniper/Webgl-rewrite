b7ec5ecb56ae5d0df3d75c0fa74ed94cd344292f
Shujiang Wu <shujiangwu@dyn147077.cc.lehigh.edu>
2018-01-30 04:39:05 -0500

    测试cal_z

--- client/fingerprint/texture/shader.fs.glsl
+++ client/fingerprint/texture/shader.fs.glsl
@@ -9,10 +9,11 @@ struct txt_p {
   int x0, y0, x1, y1, x2, y2, x3,  y3;
 };
 #define uniformNumber 336
-#define init tri_p tri; txt_p fragTexCoord; int z; z = -512;gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+#define init tri_p tri; txt_p fragTexCoord; int z; z = -512;gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);int z0;
 #define assign tri.x0 = int(gl_FragCoord.x); tri.y0 = int(gl_FragCoord.y); tri.x1 = tri_point[i][0]; tri.y1 = tri_point[i][1]; tri.z1 = tri_point[i][2]; tri.x2 = tri_point[i+1][0]; tri.y2 = tri_point[i+1][1]; tri.z2 = tri_point[i+1][2]; tri.x3 = tri_point[i+2][0]; tri.y3 = tri_point[i+2][1]; tri.z3 = tri_point[i+2][2];
 int judge(tri_p t);
-int PinAB(int tx0, int ty0, int tx1, int ty1, int tx2, int ty2);                
+int PinAB(int tx0, int ty0, int tx1, int ty1, int tx2, int ty2);   
+int cal_z(tri_p tri);             
                 
 void main()
 {
@@ -20,11 +21,13 @@ void main()
   for (int i = 0; i < uniformNumber; i+= 3){
     assign;
     if (judge(tri) == 1){
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      z0 = cal_z(tri);
+      if ( (z0 >= -512) && (z0 <= 512) && (z0 > z)){
+        z = z0;
+        gl_FragColor = vec4(float(tri.x1)/255.0, float(tri.y1)/255.0, 0.0, 1.0);
+      } 
     }
-  }
-  
-  
+  } 
 }
 
 int judge(tri_p t) {
@@ -33,4 +36,13 @@ int judge(tri_p t) {
 
 int PinAB(int tx0, int ty0, int tx1, int ty1, int tx2, int ty2){ 
 int kb, kc; kb = tx0*ty1 - tx1*ty0; kc = tx0*ty2 - tx2*ty0;if  ( ((0 > kb ) && (0 < kc )) || ((0 < kb ) && (0 > kc)) ) {return 1;} return 0; 
+}
+
+int cal_z(tri_p t){
+  int A, B, C , D , K;
+  A = (t.y3 - t.y1)*(t.z3 - t.z1) - (t.z2 - t.z1)*(t.y3 - t.y1);
+  B = (t.x3 - t.x1)*(t.z2 - t.z1) - (t.x2 - t.x1)*(t.z3 - t.z1);
+  C = (t.x2 - t.x1)*(t.y3 - t.y1) - (t.x3 - t.x1)*(t.y2 - t.y1);
+  D = -1 * (A * t.x1 + B * t.y1 + C * t.z1);
+  return (-1 * (A * t.x0 + B * t.y0 + D) / C);
 }
