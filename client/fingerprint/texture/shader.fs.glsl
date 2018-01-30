75b06beec9ccb1bf794397f0e3cce0f065de1eed
Shujiang Wu <shujiangwu@dyn147077.cc.lehigh.edu>
2018-01-30 08:12:47 -0500

    tx, ty

--- client/fingerprint/texture/shader.fs.glsl
+++ client/fingerprint/texture/shader.fs.glsl
@@ -6,11 +6,11 @@ struct tri_p {
   int x0, y0, x1, y1, z1, x2, y2, z2, x3,  y3, z3;
 };
 struct txt_p {
-  int x0, y0, x1, y1, x2, y2, x3,  y3;
+  int x1, y1, x2, y2, x3,  y3;
 };
 #define uniformNumber 336
 #define init tri_p tri; txt_p fragTexCoord; int z; z = -512;gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);int z0;
-#define assign tri.x0 = int(gl_FragCoord.x); tri.y0 = int(gl_FragCoord.y); tri.x1 = tri_point[i][0]; tri.y1 = tri_point[i][1]; tri.z1 = tri_point[i][2]; tri.x2 = tri_point[i+1][0]; tri.y2 = tri_point[i+1][1]; tri.z2 = tri_point[i+1][2]; tri.x3 = tri_point[i+2][0]; tri.y3 = tri_point[i+2][1]; tri.z3 = tri_point[i+2][2];
+#define assign tri.x0 = int(gl_FragCoord.x); tri.y0 = int(gl_FragCoord.y); tri.x1 = tri_point[i][0]; tri.y1 = tri_point[i][1]; tri.z1 = tri_point[i][2]; tri.x2 = tri_point[i+1][0]; tri.y2 = tri_point[i+1][1]; tri.z2 = tri_point[i+1][2]; tri.x3 = tri_point[i+2][0]; tri.y3 = tri_point[i+2][1]; tri.z3 = tri_point[i+2][2];fragTexCoord.x1 = text_point[i][0]; fragTexCoord.y1 = text_point[i][1];fragTexCoord.x2 = text_point[i+1][0]; fragTexCoord.y2 = text_point[i+1][1];fragTexCoord.x3 = text_point[i+2][0]; fragTexCoord.y3 = text_point[i+2][1];
 int judge(tri_p t);
 int PinAB(int tx0, int ty0, int tx1, int ty1, int tx2, int ty2);   
 int cal_z(tri_p tri);
@@ -82,7 +82,7 @@ int mod(int a, int b){
 
 
 vec4 D_texture2D(sampler2D sampler, txt_p f, tri_p t){
-  int bcs1, bcs2, bcs3, cs1, cs2, cs3, wei_1, wei_2, wei_3;
+  int bcs1, bcs2, bcs3, cs1, cs2, cs3, wei_1, wei_2, wei_3, tx, ty;
   bcs1 = (t.x0 * t.y2 + t.x2 * t.y3 + t.x3 * t.y0) - (t.x3 * t.y2 + t.x2 * t.y0 + t.x0 * t.y3);
   cs1 =  (t.x1 * t.y2 + t.x2 * t.y3 + t.x3 * t.y1) - (t.x3 * t.y2 + t.x2 * t.y1 + t.x1 * t.y3);
   wei_1 = division(bcs1 * 1000, cs1);
@@ -95,5 +95,9 @@ vec4 D_texture2D(sampler2D sampler, txt_p f, tri_p t){
   cs3 =  (t.x1 * t.y2 + t.x2 * t.y3 + t.x3 * t.y1) - (t.x3 * t.y2 + t.x2 * t.y1 + t.x1 * t.y3);
   wei_3 = division(bcs3 * 1000, cs3);
 
-  return vec4( float( mod (wei_1, 255 )) / 255.0, float( mod (wei_2, 255 )) / 255.0, float( mod (wei_3, 255 )) / 255.0, 1.0  );
+  tx = division (wei_1 * f.x1 + wei_2 * f.x2 + wei_3 * f.x3, 1000);
+  ty = division (wei_1 * f.y1 + wei_2 * f.y2 + wei_3 * f.y3, 1000);
+
+  return texture2D(sampler, vec2 ( float(tx-1)/255.0, float(ty-1)/255.0 ));
+  //return vec4( float( mod (wei_1, 255 )) / 255.0, float( mod (wei_2, 255 )) / 255.0, float( mod (wei_3, 255 )) / 255.0, 1.0  );
 }
