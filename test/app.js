7379c661f7dbf70f30e3ab2a6a599a57e2c6e380
Song-Li <lisong1013@gmail.com>
2018-04-12 19:08:38 +0000

    changed the operators to function

--- test/app.js
+++ test/app.js
@@ -24,6 +24,35 @@ var fragmentShaderText =
 '}'
 ].join('\n');
 
+var testShader = 
+`
+attribute vec3 vertPosition;
+attribute vec2 vertTexCoord;
+attribute vec3 vertNormal;
+
+varying vec2 fragTexCoord;
+varying vec3 fragNormal;
+
+uniform mat4 mWorld = mat4(2);
+uniform mat4 mView;
+uniform mat4 mProj;
+
+void main()
+{
+  fragTexCoord = vertTexCoord;
+  fragNormal = (mWorld * vec4(vertNormal, 0.0)).xyz;
+  mProj += mProj;
+  mProj /= mProj;
+  if (fragTexCoord == vertNormal) {
+    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
+    gl_Position = gl_Position + gl_Position;
+  }else {
+    gl_Position = mProj;
+    gl_Position = mProj - mView;
+  }
+}
+`
+
 var Demo = function () {
 	var canvas = document.getElementById('game-surface');
 	var gl = canvas.getContext('webgl');
@@ -33,6 +62,8 @@ var Demo = function () {
 		gl = canvas.getContext('experimental-webgl');
 	}
   var Compiler = GLSL();
-  console.log(Compiler.compile(fragmentShaderText));
-  console.log(Compiler.compile(vertexShaderText));
+  //console.log(Compiler.compile(fragmentShaderText));
+  //console.log(Compiler.compile(vertexShaderText));
+  console.log(testShader);
+  console.log(Compiler.compile(testShader));
 }
