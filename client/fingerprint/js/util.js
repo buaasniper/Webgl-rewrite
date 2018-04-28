331e91afd914c872b434ba0a56282a5df57b4951
Shujiang Wu <shujiangwu@dyn144167.cc.lehigh.edu>
2018-04-28 16:59:11 -0400

    自动化连接版本

--- client/fingerprint/js/util.js
+++ client/fingerprint/js/util.js
@@ -796,7 +796,7 @@ Mat3 = (function() {
 		//console.log("ProgramDataMap", ProgramDataMap);
 
 
-		var parsingflag = 0;
+		var parsingflag = 1;
 if (parsingflag == 1){
 		/*------------------自动化连接部分------------------------------------*/
 		/*------------------数据输入部分--------------------------------------*/
@@ -932,12 +932,12 @@ if (parsingflag == 1){
 	   	//console.log("compiled",compiled);
 		eval(compiled);
 
-		console.log("tmp", math.flatten(tmp._data));
-		console.log("final_tmp", math.flatten(final_tmp._data));
-		console.log("vPosition",vPosition);
-		console.log("fragTexCoord",fragTexCoord);
-		console.log("fragNormal",fragNormal);
-		console.log("gl_Position",gl_Position);
+		// console.log("tmp", math.flatten(tmp._data));
+		// console.log("final_tmp", math.flatten(final_tmp._data));
+		// console.log("vPosition",vPosition);
+		// console.log("fragTexCoord",fragTexCoord);
+		// console.log("fragNormal",fragNormal);
+		// console.log("gl_Position",gl_Position);
 
 
 		/*------------------数据输入部分--------------------------------------*/
@@ -1336,14 +1336,14 @@ if (parsingflag == 1){
 			var vertNormal = [];
 			var varyingmap = [];
 			//attribute 读取阶段
-			for (var i = 0; i < ProgramDataMap[activeProgramNum].attriData.length; i++){
-				if (ProgramDataMap[activeProgramNum].attriData[i].shaderName == "vertPosition")
-					vertPosition = ProgramDataMap[activeProgramNum].attriData[i].uniformData;					
-				if (ProgramDataMap[activeProgramNum].attriData[i].shaderName == "vertTexCoord")
-					vertTexCoord = ProgramDataMap[activeProgramNum].attriData[i].uniformData;
-				if (ProgramDataMap[activeProgramNum].attriData[i].shaderName == "vertNormal")
-					vertNormal = ProgramDataMap[activeProgramNum].attriData[i].uniformData;
-			}
+			// for (var i = 0; i < ProgramDataMap[activeProgramNum].attriData.length; i++){
+			// 	if (ProgramDataMap[activeProgramNum].attriData[i].shaderName == "vertPosition")
+			// 		vertPosition = ProgramDataMap[activeProgramNum].attriData[i].uniformData;					
+			// 	if (ProgramDataMap[activeProgramNum].attriData[i].shaderName == "vertTexCoord")
+			// 		vertTexCoord = ProgramDataMap[activeProgramNum].attriData[i].uniformData;
+			// 	if (ProgramDataMap[activeProgramNum].attriData[i].shaderName == "vertNormal")
+			// 		vertNormal = ProgramDataMap[activeProgramNum].attriData[i].uniformData;
+			// }
 			//uniform 读取阶段
 			for (var i = 0; i < ProgramDataMap[activeProgramNum].uniformData.length; i++){
 				if (ProgramDataMap[activeProgramNum].uniformData[i].shaderName == "mWorld")
@@ -1377,13 +1377,13 @@ if (parsingflag == 1){
 			var newData1 = new Varying_data;
 			newData1.shaderName = "tri_point";
 			newData1.varyEleNum = 3;
-			newData1.uniformData = my_m4.vec_max_mul(vertPosition, mWorld);
+			// newData1.uniformData = my_m4.vec_max_mul(vertPosition, mWorld);
 			// console.log("原始值",my_m4.vec_max_mul(vertPosition, mWorld));
 			//console.log(gl_Position);
-			// newData1.uniformData = handle_gl_Position(gl_Position);
-			// console.log(newData1.uniformData);
-			// newData1.uniformData = math.flatten(newData1.uniformData);
-			// console.log(newData1.uniformData);
+			newData1.uniformData = handle_gl_Position(gl_Position);
+			console.log(newData1.uniformData);
+			newData1.uniformData = math.flatten(newData1.uniformData);
+			console.log(newData1.uniformData);
 			for (var i =0; i < newData1.uniformData.length; i++)
 				if (i % 3 != 2)
 					newData1.uniformData[i] = Math.floor((newData1.uniformData[i] + 1) * 128);
@@ -1398,15 +1398,15 @@ if (parsingflag == 1){
 			var newData2 = new Varying_data;
 			newData2.shaderName = "text_point";
 			newData2.varyEleNum = 2;
-			for (var i = 0; i < vertTexCoord.length; i++){
-				newData2.uniformData = newData2.uniformData.concat(vertTexCoord[i]);
-				newData2.uniformData[i] = Math.floor(((newData2.uniformData[i] )) * 1000);
-			}
-			// fragTexCoord = math.flatten(fragTexCoord);
-			// for (var i = 0; i < fragTexCoord.length; i++){
-			// 	newData2.uniformData = newData2.uniformData.concat(fragTexCoord[i]);
+			// for (var i = 0; i < vertTexCoord.length; i++){
+			// 	newData2.uniformData = newData2.uniformData.concat(vertTexCoord[i]);
 			// 	newData2.uniformData[i] = Math.floor(((newData2.uniformData[i] )) * 1000);
-			// }		
+			// }
+			fragTexCoord = math.flatten(fragTexCoord);
+			for (var i = 0; i < fragTexCoord.length; i++){
+				newData2.uniformData = newData2.uniformData.concat(fragTexCoord[i]);
+				newData2.uniformData[i] = Math.floor(((newData2.uniformData[i] )) * 1000);
+			}		
 			ProgramDataMap[activeProgramNum].varyingData.push(newData2);
 			//console.log("vertTexCoord",vertTexCoord);
 			//console.log("newData2",newData2);
@@ -1419,15 +1419,15 @@ if (parsingflag == 1){
 				newData3.shaderName = "nor_point";
 				newData3.varyEleNum = 3;
 				//只是一个中间替代值为了让后面的值不变
-				var tem = [];
-				for (var i = 0; i < vertNormal.length; i++)
-					tem = tem.concat(vertNormal[i]);
-				for (var i = 0; i < tem.length; i += 3){
-					newData3.uniformData = newData3.uniformData.concat((tem[i] * mWorld_fs[0] + tem[i+1] * mWorld_fs[4] + tem[i+2] * mWorld_fs[8]));
-					newData3.uniformData = newData3.uniformData.concat((tem[i] * mWorld_fs[1] + tem[i+1] * mWorld_fs[5] + tem[i+2] * mWorld_fs[9]) );
-					newData3.uniformData = newData3.uniformData.concat((tem[i] * mWorld_fs[2] + tem[i+1] * mWorld_fs[6] + tem[i+2] * mWorld_fs[10])) ;
-				}
-				console.log(newData3);
+				// var tem = [];
+				// for (var i = 0; i < vertNormal.length; i++)
+				// 	tem = tem.concat(vertNormal[i]);
+				// for (var i = 0; i < tem.length; i += 3){
+				// 	newData3.uniformData = newData3.uniformData.concat((tem[i] * mWorld_fs[0] + tem[i+1] * mWorld_fs[4] + tem[i+2] * mWorld_fs[8]));
+				// 	newData3.uniformData = newData3.uniformData.concat((tem[i] * mWorld_fs[1] + tem[i+1] * mWorld_fs[5] + tem[i+2] * mWorld_fs[9]) );
+				// 	newData3.uniformData = newData3.uniformData.concat((tem[i] * mWorld_fs[2] + tem[i+1] * mWorld_fs[6] + tem[i+2] * mWorld_fs[10])) ;
+				// }
+				// console.log(newData3);
 
 				 fragNormal = math.flatten(fragNormal);
 				 console.log(fragNormal);
@@ -1444,19 +1444,19 @@ if (parsingflag == 1){
 				newData4.shaderName = "vPosition";
 				newData4.varyEleNum = 4;
 				//只是一个中间替代值为了让后面的值不变
-				var tem = [];
-				for (var i = 0; i < vertPosition.length; i++)
-					tem = tem.concat(vertPosition[i]);
-				for (var i = 0; i < tem.length; i += 3){
-					newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[0] + tem[i+1] * mView_fs[4] + tem[i+2] * mView_fs[8] +  mView_fs[12]));
-					newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[1] + tem[i+1] * mView_fs[5] + tem[i+2] * mView_fs[9] +  mView_fs[13]));
-					newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[2] + tem[i+1] * mView_fs[6] + tem[i+2] * mView_fs[10]+  mView_fs[14]));
-					newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[3] + tem[i+1] * mView_fs[7] + tem[i+2] * mView_fs[11]+  mView_fs[15]));
-				}
+				// var tem = [];
+				// for (var i = 0; i < vertPosition.length; i++)
+				// 	tem = tem.concat(vertPosition[i]);
+				// for (var i = 0; i < tem.length; i += 3){
+				// 	newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[0] + tem[i+1] * mView_fs[4] + tem[i+2] * mView_fs[8] +  mView_fs[12]));
+				// 	newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[1] + tem[i+1] * mView_fs[5] + tem[i+2] * mView_fs[9] +  mView_fs[13]));
+				// 	newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[2] + tem[i+1] * mView_fs[6] + tem[i+2] * mView_fs[10]+  mView_fs[14]));
+				// 	newData4.uniformData = newData4.uniformData.concat((tem[i] * mView_fs[3] + tem[i+1] * mView_fs[7] + tem[i+2] * mView_fs[11]+  mView_fs[15]));
+				// }
 
-				// vPosition = math.flatten(vPosition);
-				// for (var i = 0; i < vPosition.length; i++)
-				//  	newData4.uniformData = newData4.uniformData.concat(vPosition[i]);
+				vPosition = math.flatten(vPosition);
+				for (var i = 0; i < vPosition.length; i++)
+				 	newData4.uniformData = newData4.uniformData.concat(vPosition[i]);
 
 				for (var i = 0; i < newData4.uniformData.length; i++)
 					newData4.uniformData[i] = Math.round(((newData4.uniformData[i] )) * 1000);
